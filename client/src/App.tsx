56c0934cd2f20bb53fb6b93a681e3552514137b5
raph <raph37heth@gmail.com>
2019-10-14 17:56:13 +0100

    first commit!

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      
+      {/*  App goes here :) */}
+
+    </div>
+  );
+}
+
+export default App;
